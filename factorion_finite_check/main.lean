32c55f5b28d242434a50deba0449df8e0cd7d676
David Lopez <146124340+dalopezt@users.noreply.github.com>
2026-05-08 22:15:02 -0600

    Create computation node factorion_finite_check_computation under factorion_finite_check (c292f845) via CoProof

--- factorion_finite_check/main.lean
+++ factorion_finite_check/main.lean
@@ -1,4 +1,4 @@
+import «factorion_finite_check_computation».«main»
 import Definitions
-
 theorem factorion_finite_check : ∀ n : ℕ, n ≤ 2540160 → (isFactorion n ↔ n ∈ ({1, 2, 145, 40585} : Set ℕ)) := by
-  sorry
+  simpa using factorion_finite_check_computation
